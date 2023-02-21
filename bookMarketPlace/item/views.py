35d495e0ba15939cd729457fead2e54982a41a1a
anggawidiarta <anggawidiarta55@gmail.com>
2023-02-21 12:41:02 +0800

    create signup page and function

--- bookMarketPlace/item/views.py
+++ bookMarketPlace/item/views.py
@@ -4,4 +4,9 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
-    return render(request, "item/detail.html", {"item": item})
+    related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(
+        pk=pk
+    )[0:3]
+    return render(
+        request, "item/detail.html", {"item": item, "related_items": related_items}
+    )
