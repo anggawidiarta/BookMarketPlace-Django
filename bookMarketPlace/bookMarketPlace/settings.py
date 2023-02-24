51e24575783873128b8cd8b813e60c0809fefb15
anggawidiarta <anggawidiarta55@gmail.com>
2023-02-24 12:22:44 +0800

    dashboard update

--- bookMarketPlace/bookMarketPlace/settings.py
+++ bookMarketPlace/bookMarketPlace/settings.py
@@ -27,7 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGIN_URL = "/login/"
 
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
 # Application definition
 
 INSTALLED_APPS = [
@@ -39,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "core",
     "item",
+    "dashboard",
 ]
 
 MIDDLEWARE = [
